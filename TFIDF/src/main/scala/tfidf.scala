8823bc4811309cf003548c2acbd7c3fd9b1cc668
Melina Moraiti <melinamoraith@gmail.com>
2024-06-10 15:22:29 +0300

    Translate comments to greek

--- TFIDF/src/main/scala/tfidf.scala
+++ TFIDF/src/main/scala/tfidf.scala
@@ -5,49 +5,39 @@ import org.apache.spark.sql.{SQLContext, Row}
 import org.apache.spark.sql.functions._
 
 object tfidf {
-
   def main(args: Array[String]): Unit = {
-    // Check if the input path is provided
+    // Ελεγχος αν ο κατάλογος εισόδου έχει δοθεί ως όρισμα απο τη γραμμή εντολών
     if (args.length < 1) {
       println("Usage: ./run-spark.sh <File_with_main_class> <number_of_threads> <name_of_jar_file> <fileset path>")
       System.exit(1)
     }
-    // Use the input path from command line arguments
+    // Χρηση της διαδρομής εισόδου από τα ορίσματα γραμμής εντολών
     val inputPath = args(0)
-    // Use the output path from command line arguments
-    //val outputPath = args(1)
-    // Create spark configuration
+    // Spark Setup
     val sparkConf = new SparkConf()
       .setMaster("local[2]")
       .setAppName("TFIDF")
-
-    // Create spark context  
+    // Δημιουρία του περιβάλλοντος εκτέλεσης του Spark
     val sc = new SparkContext(sparkConf) 
-    // Create SQLContext
+    // Δημιουργία του περιβάλλοντος SQL
     val sqlContext = new SQLContext(sc)
-    // Read documents from input path
+    // Διαβάζει τα αρχεία απο τον κατάλογο απο τη διαδρομή που δόθηκε ως είσοδος
     val documents = sc.textFile(s"file:///$inputPath").map(_.split(" ").toSeq)
-    
-    // Convert RDD to DataFrame
-   import sqlContext.implicits._
+    // Μετατροπή του RDD σε DataFrame
+    import sqlContext.implicits._
     val data = documents.zipWithIndex.map { case (words, id) => (id, words.mkString(" ")) }.toDF("label", "file_contend")
-
-    // Tokenize text into words
+    // Διαχωρίζει το κείμενο κάθε αρχείου  σε tokens (΄΄ορους)
     val tokenizer = new Tokenizer().setInputCol("file_content").setOutputCol("words")
     val wordsData = tokenizer.transform(data)
-    
-    // Apply HashingTF to convert words into feature vectors
+    // Το HashingTF μετατρέπει τα tokens σε σε διανύσματα χαρακτηριστικών
     val hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(12) 
     val featurizedData = hashingTF.transform(wordsData)
-
-    // Compute IDF
+    // Υπολογισμός του IDF
     val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
     val idfModel = idf.fit(featurizedData)
     val rescaledData = idfModel.transform(featurizedData)
-
-    // Show all results without truncating
-    readableData.select("label", "features").show(truncate = false)
-
+    // Εμφάνιση όλων των αποτελεσμάτων χωρίς περικοπή
+    rescaledData.select("label", "features").show(truncate = false)
     sc.stop()
   }
-} 
+}
